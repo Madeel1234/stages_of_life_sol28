e96a993529f17c0e1ee28d56b74aa0b0a17b3e00
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 17:34:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+// Task # 28     (2,4,13,20,65.....elder)
+let age = 35;
+if (age < 2) {
+    console.log("you are a baby");
+}
+else if (age < 4) {
+    console.log("you are a toddler");
+}
+else if (age < 13) {
+    console.log("kid");
+}
+else if (age < 20) {
+    console.log("you are teenager");
+}
+else if (age < 65) {
+    console.log("you are adult ");
+}
+else {
+    console.log("you are older");
+}
+export {};
